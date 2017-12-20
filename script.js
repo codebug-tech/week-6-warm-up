6d24a200271aca2c5bc6ebde87af98e22cda7e2d
Brandon Sorbara <sorbarbj@Brandons-MBP-2.home>
2017-12-19 20:32:17 -0500

    week 6 warmup

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
-document.addEventListener("DOMContentLoaded", function() { 
+document.addEventListener("DOMContentLoaded", function() {
   // write your javascript code below.
-  
 
+  var redo = true;
 
+  while (redo) {
+    var snowAnswer = prompt("Do you like snow?");
+        if (snowAnswer == "Yes") {
+          alert("YAY! Let's go watch the movie Elf and talk about Christmas!!");
+          redo = false;
+        }
+        else if (snowAnswer == "No") {
+          alert("MOVE DOWN TO NASHVILLE THEN AND DON'T COME BACK!!");
+          redo = false;
+        }
+        else {
+          alert("You did not enter a valid response. Please try again.");
+        };
+  };
 
-
-
-
-
-
-
-
-
-
-  
 });
