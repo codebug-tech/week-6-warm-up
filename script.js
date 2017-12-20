2172612ef32a527f9dfd1ef86ee700dff0bbed0b
Your Name <cbetack@gmail.com>
2017-12-19 19:17:42 -0600

    cbetack

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-document.addEventListener("DOMContentLoaded", function() { 
+document.addEventListener("DOMContentLoaded", function() {
   // write your javascript code below.
-  
+  var snow = prompt("Do you like snow?");
+  if (snow == "yes") {
+    alert ("yay!!");
+  } else if (snow == "no") {
+    alert ("Boo!!")
+  } else {
+    alert ("Your response is invalid");
+  }
 
 
 
@@ -13,5 +20,5 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 
-  
+
 });
