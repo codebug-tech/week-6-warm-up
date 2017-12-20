cb8a2e4b881f4e87d633607ab96178f713f4513c
Brandon Sorbara <sorbarbj@Brandons-MBP-2.home>
2017-12-19 20:46:27 -0500

    new warmup week 6

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
 document.addEventListener("DOMContentLoaded", function() {
   // write your javascript code below.
 
-  var redo = true;
+//   var redo = true;
 
-  while (redo) {
-    var snowAnswer = prompt("Do you like snow?");
-        if (snowAnswer == "Yes") {
-          alert("YAY! Let's go watch the movie Elf and talk about Christmas!!");
-          redo = false;
-        }
-        else if (snowAnswer == "No") {
-          alert("MOVE DOWN TO NASHVILLE THEN AND DON'T COME BACK!!");
-          redo = false;
-        }
-        else {
-          alert("You did not enter a valid response. Please try again.");
-        };
-  };
+//   while (redo) {
+//     var snowAnswer = prompt("Do you like snow?");
+//         if (snowAnswer == "Yes") {
+//           alert("YAY! Let's go watch the movie Elf and talk about Christmas!!");
+//           redo = false;
+//         }
+//         else if (snowAnswer == "No") {
+//           alert("MOVE DOWN TO NASHVILLE THEN AND DON'T COME BACK!!");
+//           redo = false;
+//         }
+//         else {
+//           alert("You did not enter a valid response. Please try again.");
+//         };
+//   };
+
+// takes two parameters and multiples them together
+
+function productOfTwoNumbers(num1, num2) {
+  return num1 * num2;
+};
+
+console.log(productOfTwoNumbers(5,7))
 
 });
